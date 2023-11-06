44fdd256e594a1e9cc7700cffce66ecabba3a9f6
AGRAWAL Deepak (ext) <u572229@bpost.be>
2023-11-06 21:18:42 +0100

    intial structure of bowling game kata

---
+++ src/test/java/bowling/game/kata/BowlingGameKataApplicationTests.java
@@ -0,0 +1,13 @@
+package bowling.game.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingGameKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
